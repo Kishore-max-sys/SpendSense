e5e8edff4685dd04d3507c509dc71e40491312b2
Kishore <seerlayuvasrinivaskishore@gmail.com>
2026-08-09 02:02:34 +0530

    debugging

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
 const errorHandler=(error,req,res,next) =>{
+    console.error("ERROR:", err);
     res.status(error.statusCode || 500).json({
             success:false,
             message:error.message || "Internal server error"
